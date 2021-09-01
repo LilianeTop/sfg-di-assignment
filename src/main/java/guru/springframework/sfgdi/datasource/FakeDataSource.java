3d1570813d2e9fe03a9162f5fe03b63aed0a187e
Liliane Top <Liliane.top@procura.nl>
2021-09-01 15:04:51 +0200

    created database.properties file to set bd access variables

---
+++ src/main/java/guru/springframework/sfgdi/datasource/FakeDataSource.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgdi.datasource;
+
+
+import lombok.Data;
+
+@Data
+public class FakeDataSource {
+
+    private String username;
+    private String password;
+    private String jdbcurl;
+
+
+}
